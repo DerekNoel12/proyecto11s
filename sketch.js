8d2da3cb0189a1d45b1e01011b72f28841ab6a54
DerekNoel12 <94418272+DerekNoel12@users.noreply.github.com>
2021-11-21 11:20:43 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,18 +1,66 @@
-var weight = [35,38,42,45,43,34,36,41,48,32];
-var suma=0
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
 
-function setup() {
+function preload(){
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+}
+
+function setup(){
+  
   createCanvas(400,400);
-for(var i=0; i<weight.length; i++) { 
-  suma=suma+weight[1];}
+  
+// Mover el fondo
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+path.scale=1.2;
 
-var promedio=suma/weight.length;
-console.log(promedio)
+//crear sprite de niño corriendo
+boy = createSprite(180,340,30,30);
+boy.scale=0.08;
+boy.addAnimation("JakeRunning",boyImg);
+  
+
+leftBoundary=createSprite(0,0,100,800);
+
+// leftBoundary.invisible = false;
+// leftBoundary.visible = true;
+// leftBoundary.invisible = true;
+// leftBoundary.visible = false;
+
+
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
 }
-function draw() 
-{
-  background(30);
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
+  
+  //código para reiniciar el fondo
+
+  /*if(path.y > 400 ){
+    path.y = height/2;
+  }*/
+
+  /*if(path.y > 400 ){
+   
+  path.y = height/2;
+  }*/
+
+  /*if(path.y > 400 ){
+path.y = height/2;}*/
+
+/*if(path.y > 400 ){path.y = height/2;}*/
+  
+  drawSprites();
 }
-
- 
-
